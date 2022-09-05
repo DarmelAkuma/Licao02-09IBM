9a08fd27df0fac1bc9b4331d3a226337830dc762
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-09-05 12:02:53 -0300

    Commit inicial

---
+++ ContCaracter/ContCaracter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContCaracter {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.WriteLine("Digite a mensagem: ");
+            string[] frase;
+            string Palavra = "";
+
+            for (; ; )
+            {
+                frase = Console.ReadLine().Split(' ');
+                if (frase[0] == "0") {                  
+                    break;
+                }
+                string numLetras = "";
+
+                for (int i = 0; i < frase.Length; i++) {
+                    if (frase[i].Length >= Palavra.Length) {
+                        Palavra = frase[i];
+                    }
+                    if (i == (frase.Length - 1)) {
+                        numLetras += $"{frase[i].Length}";
+                    }
+                    else {
+                        numLetras += $"{frase[i].Length}-";
+                    }
+                }
+
+                Console.WriteLine("Numeros de caracteres: " + numLetras);
+                Console.WriteLine("The biggest word: " + Palavra);
+            }
+        }
+    }
+}
